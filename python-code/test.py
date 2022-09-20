03e2c14bbe25328f0b476a7b329bfb103ff11062
Alan I-Shref (LanPodder) <alan.ishref@valtech.com>
2022-09-20 12:15:35 +0200

    works with requests.get

--- python-code/test.py
+++ python-code/test.py
@@ -3,8 +3,14 @@ from os import environ
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
+import requests
+
 secret = SecretClient(
     vault_url=environ["KEYVAULT"], credential=DefaultAzureCredential()
 ).get_secret("my-secret")
 data = str(secret.value)
 print(data)
+
+x = requests.get(f'{environ["KEYVAULT"]}/secrets/my-secret/?api-version=7.3')
+print(x.status_code)
+print(x.json())
